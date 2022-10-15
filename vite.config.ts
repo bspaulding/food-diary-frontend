eea01a9579520f0bc93ab2508c7ad56194b8a90a
Bradley J. Spaulding <brad.spaulding@gmail.com>
2022-10-15 10:45:45 -0700

    Defaulting vite to local backend instead

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://food-diary.motingo.com",
+        target: "http://localhost:8080/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
