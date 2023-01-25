1246d41367292e20ef38ea32777491e76a4d0d47
Bradley J. Spaulding <brad.spaulding@gmail.com>
2023-01-24 21:01:41 -0800

    updated recipe show page

--- src/RecipeShow.tsx
+++ src/RecipeShow.tsx
@@ -1,4 +1,5 @@
 import type { Component } from "solid-js";
+import { createMemo, Index } from "solid-js";
 import { useParams, Link } from "@solidjs/router";
 import createAuthorizedResource from "./createAuthorizedResource";
 import { fetchRecipe } from "./Api";
@@ -8,14 +9,27 @@ const RecipeShow: Component = () => {
   const [recipeQuery] = createAuthorizedResource(() => params.id, fetchRecipe);
   const recipe = () =>
     recipeQuery()?.data?.food_diary_recipe_by_pk || { id: params.id };
+  const recipeItems = createMemo(() => recipe().recipe_items);
 
   return (
     <>
       <p>
         <Link href="/">Back to entries</Link>
+      </p>
+      <p>
         <Link href={`/recipe/${params.id}/edit`}>Edit</Link>
       </p>
-      <pre>{JSON.stringify(recipe(), null, 2)}</pre>
+      <h1 class="text-lg font-semibold">{recipe().name}</h1>
+      <Index each={recipeItems()} fallback="No recipe items.">
+        {(item) => (
+          <li class="list-none my-1">
+            <Link href={`/nutrition_item/${item().nutrition_item.id}`}>
+              {item().nutrition_item.description}
+            </Link>
+            <p class="text-sm">{item().servings} servings</p>
+          </li>
+        )}
+      </Index>
     </>
   );
 };
