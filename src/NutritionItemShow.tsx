174fe5b78430976e9ae134f974d6d6ac3dec5e58
Bradley J. Spaulding <brad.spaulding@gmail.com>
2023-03-03 18:02:13 -0800

    prettier nutrition item page

--- src/NutritionItemShow.tsx
+++ src/NutritionItemShow.tsx
@@ -2,6 +2,34 @@ import type { Component } from "solid-js";
 import { useParams, Link } from "@solidjs/router";
 import { fetchNutritionItem } from "./Api";
 import createAuthorizedResource from "./createAuthorizedResource";
+import ButtonLink from "./ButtonLink";
+
+const keys_denylist = ["id", "description"];
+
+const propTitles = {
+  calories: "Calories",
+  totalFatGrams: "Total Fat (g)",
+  saturatedFatGrams: "Saturated Fat (g)",
+  transFatGrams: "Trans Fat (g)",
+  polyunsaturatedFatGrams: "Polyunsaturated Fat (g)",
+  monounsaturatedFatGrams: "Monounsaturated Fat (g)",
+  cholesterolMilligrams: "Cholesterol (mg)",
+  sodiumMilligrams: "Sodium (mg)",
+  totalCarbohydrateGrams: "Total Carbohydrate (g)",
+  dietaryFiberGrams: "Dietary Fiber (g)",
+  totalSugarsGrams: "Total Sugars (g)",
+  addedSugarsGrams: "Added Sugars (g)",
+  proteinGrams: "Protein (g)",
+};
+
+const boldKeys = [
+  "calories",
+  "totalFatGrams",
+  "cholesterolMilligrams",
+  "sodiumMilligrams",
+  "totalCarbohydrateGrams",
+  "proteinGrams",
+];
 
 const NutritionItemShow: Component = () => {
   const params = useParams();
@@ -14,13 +42,25 @@ const NutritionItemShow: Component = () => {
     nutritionItemQuery()?.data?.food_diary_nutrition_item_by_pk || {};
   return (
     <div style={{ margin: "18px" }}>
-      <p>
-        <Link href="/">Back to entries</Link>
-      </p>
-      <p>
-        <Link href={`/nutrition_item/${params.id}/edit`}>Edit</Link>
-      </p>
-      <pre>{JSON.stringify(nutritionItem(), null, 2)}</pre>
+      <div class="flex space-x-4 mb-4">
+        <ButtonLink href="/">Back to Diary</ButtonLink>
+        <ButtonLink href={`/nutrition_item/${params.id}/edit`}>
+          Edit Item
+        </ButtonLink>
+      </div>
+      <h1 class="font-semibold text-2xl">{nutritionItem().description}</h1>
+      <div class="text-lg">
+        {Object.keys(nutritionItem())
+          .filter((k) => keys_denylist.indexOf(k) < 0)
+          .map((k) => (
+            <p class="flex justify-between">
+              <span class={boldKeys.indexOf(k) >= 0 ? "font-semibold" : "ml-4"}>
+                {propTitles[k] || k}
+              </span>
+              <span>{nutritionItem()[k]}</span>
+            </p>
+          ))}
+      </div>
     </div>
   );
 };
